f63bb4e608b1e29f3a5e0d24a739ede66530d8a8
Dewin J. Martínez <dewin.martinez@gmail.com>
2017-11-10 18:17:46 -0400

    Add best-practices and variable rules unit test

--- test/test.js
+++ test/test.js
@@ -4,7 +4,6 @@ const assert = require('assert');
 const eslint = require('eslint');
 const conf = require('../');
 const cliEngine = new eslint.CLIEngine(conf);
-const linter = require("eslint").linter;
 
 // The source files to lint.
 const repoFiles = [
@@ -21,7 +20,20 @@ repoFiles.forEach((file, index) => {
   assert(repoReport.results[index].filePath.endsWith(file));
 });
 
+// best-practices rules Unit Test
+// validate no-empty-function rule
+const bestPracticeReport = cliEngine.executeOnText("function foo() {} foo()");
+assert.equal(bestPracticeReport.warningCount, 0);
+assert.equal(bestPracticeReport.errorCount, 1); //should fail when has a error
+
 // possible-error rules Unit Test
 // validate use of console: rules: { 'no-console': 'warn' }
-let possibleErrorReport = cliEngine.executeOnText("console.log(the);");
-assert.equal(possibleErrorReport.warningCount, 1);
+const possibleErrorReport = cliEngine.executeOnText("console.log(the);");
+assert.equal(possibleErrorReport.warningCount, 1); //should fail when has a warn
+assert.equal(repoReport.errorCount, 0);
+
+// variable rules Unit Test
+// validate no-unused-vars rule
+const variableReport = cliEngine.executeOnText("var foo = 1;");
+assert.equal(variableReport.warningCount, 0);
+assert.equal(variableReport.errorCount, 1); //should fail when has a error
